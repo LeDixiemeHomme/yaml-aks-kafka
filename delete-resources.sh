0b1a5d3712e9904f7dd0b0ae8ae09fdca2a4052c
valle <benoit.valleea@hotmail.fr>
2021-06-15 23:53:15 +0200

    add sh utils

---
+++ delete-resources.sh
@@ -0,0 +1,7 @@
+kubectl delete --all pods namespaces
+kubectl delete --all services namespaces
+kubectl delete --all deployments namespaces
+kubectl delete --all services namespaces
+kubectl delete --all pods --namespace=default
+kubectl delete --all all --namespace=default
+kubectl delete --all service --namespace=default
