bcdb85f69cf2dea5a9e2b5c653a9641e7c41534a
valle <benoit.valleea@hotmail.fr>
2021-06-15 22:48:38 +0200

    modify start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 kubectl create namespace uparser-kafka-queue
-kubectl apply -f strimzi-cluster-operator.yaml
+kubectl apply -f strimzi-cluster-operator.yaml -n uparser-kafka-queue
 kubectl apply -f create-kafka-cluster.yaml
 kubectl apply -f create-kafka-topics.yaml
 kubectl apply -f create-kafka-users.yaml
