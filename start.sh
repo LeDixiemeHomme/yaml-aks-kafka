d4b5c7bf500a696021428658d0be00dfd8d7e4a8
valle <benoit.valleea@hotmail.fr>
2021-06-15 22:45:01 +0200

    modify start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 kubectl create namespace uparser-kafka-queue
-curl -L https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.15.0/strimzi-cluster-operator-0.15.0.yaml \
-  | sed 's/namespace: .*/namespace: uparser-kafka-queue/' \
-  | kubectl apply -f - -n uparser-kafka-queue
+kubectl apply -f strimzi-cluster-operator.yaml
 kubectl apply -f create-kafka-cluster.yaml
 kubectl apply -f create-kafka-topics.yaml
 kubectl apply -f create-kafka-users.yaml
