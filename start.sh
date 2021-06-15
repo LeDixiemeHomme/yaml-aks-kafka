f2e8a50e34b1e4be6f0a9df3f507f595834d7370
valle <benoit.valleea@hotmail.fr>
2021-06-15 22:30:13 +0200

    add strimzi dependence

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
+curl -L https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.15.0/strimzi-cluster-operator-0.15.0.yaml \
+  | sed 's/namespace: .*/namespace: tls-kafka/' \
+  | kubectl apply -f - -n tls-kafka
 kubectl create namespace uparser-kafka-queue
 kubectl apply -f create-kafka-cluster.yaml
 kubectl apply -f create-kafka-topics.yaml
